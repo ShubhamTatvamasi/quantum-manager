900b90191c5b3356760c0f736ca4d6ed4106d989
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2023-11-22 18:36:43 +0000

    modified:   internal/controller/keyrequest_controller.go

--- internal/controller/keyrequest_controller.go
+++ internal/controller/keyrequest_controller.go
@@ -40,6 +40,7 @@ type KeyRequestReconciler struct {
 //+kubebuilder:rbac:groups=quantum-manager.io,resources=keyrequests,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=quantum-manager.io,resources=keyrequests/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=quantum-manager.io,resources=keyrequests/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
